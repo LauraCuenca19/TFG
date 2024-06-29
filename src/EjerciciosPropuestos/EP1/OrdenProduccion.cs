2d2ab656af0ccda3ca1eeade3211d3e840f13b37
Laura Cuenca <lauracucalvo@gmail.com>
2024-06-29 15:44:01 +0200

    EP1

---
+++ src/EjerciciosPropuestos/EP1/OrdenProduccion.cs
@@ -0,0 +1,68 @@
+﻿namespace EP1;
+
+public class OrdenProduccion
+{
+    private int id;
+    private string producto;
+    private string cliente;
+    private int cantidad;
+    private string estado;
+
+    private static int contadorOrdenes = 0;
+
+    public OrdenProduccion(string producto, string cliente, int cantidad, string estado)
+    {
+        contadorOrdenes ++;
+        id = contadorOrdenes;
+        this.producto = producto;
+        this.cliente = cliente;
+        this.cantidad = cantidad;
+        ModificarEstado(estado);
+        Console.WriteLine("Orden de Producción creada.");
+    }
+
+    public int ConsultarIdOrden()
+    {
+        return id;
+    }
+
+    public string ConsultarProducto()
+    {
+        return producto;
+    }
+
+    public string ConsultarCliente()
+    {
+        return cliente;
+    }
+
+    public int ConsultarCantidad()
+    {
+        return cantidad;
+    }
+
+    public void ModificarCantidad(int cantidad)
+    {
+        this.cantidad = cantidad;
+    }
+
+    public string ConsultarEstadoActual()
+    {
+        return estado;
+    }
+
+    public void ModificarEstado(string estado)
+    {
+        if (estado != "Pendiente" && estado != "En Proceso" && estado != "Pausada" && estado != "Completada")
+        {
+            Console.WriteLine("El estado no es válido. Se establecerá la orden como 'Pendiente'");
+            this.estado = "Pendiente";
+        } 
+        else this.estado = estado;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {id}, Producto: {producto}, Cliente: {cliente}, Cantidad: {cantidad}, Estado: {estado}";
+    }
+}
---
+++ src/EjerciciosPropuestos/EP1/Program.cs
@@ -0,0 +1,65 @@
+using EP1;
+
+class Program
+    {
+        static void Main()
+        {
+            // Lista de órdenes de producción
+            List<OrdenProduccion> ordenes = new List<OrdenProduccion>();
+
+            // Instancias de la clase OrdenProduccion
+            OrdenProduccion orden1 = new OrdenProduccion("Producto A", "Cliente 1", 550, "Pendiente");
+            OrdenProduccion orden2 = new OrdenProduccion("Producto B", "Cliente 2", 450, "Pausada");
+            OrdenProduccion orden3 = new OrdenProduccion("Producto C", "Cliente 1", 625, "En Proceso");
+            OrdenProduccion orden4 = new OrdenProduccion("Producto A", "Cliente 3", 250, "Completada");
+            OrdenProduccion orden5 = new OrdenProduccion("Producto C", "Cliente 2", 425, "Urgente");
+
+            // Añadir órdenes a la lista
+            ordenes.Add(orden1);
+            ordenes.Add(orden2);  
+            ordenes.Add(orden3);  
+            ordenes.Add(orden4);  
+            ordenes.Add(orden5);  
+
+            // Mostrar órdenes incialmente
+            Console.WriteLine("\nÓrdenes de producción iniciales:");
+            foreach (var orden in ordenes)
+            {
+                Console.WriteLine(orden.ToString());
+            }
+            
+            // Modificar algunas órdenes de producción
+            foreach (var orden in ordenes)
+            {
+                if (orden.ConsultarCantidad() >= 500)
+                {
+                    orden.ModificarCantidad(500);
+                }
+                switch (orden.ConsultarEstadoActual())
+                {
+                    case "Pendiente":
+                        orden.ModificarEstado("En Proceso");
+                        break;
+                    case "En Proceso":
+                        orden.ModificarEstado("Completada");
+                        break;
+                    case "Pausada":
+                        orden.ModificarEstado("En Proceso");
+                        break;
+                    case "Completada":
+                        orden.ModificarEstado("Completada");
+                        break;
+                    default:
+                        orden.ModificarEstado("En Proceso");
+                        break;
+                }
+            }
+
+            // Mostrar lista final
+            Console.WriteLine("\nLista de ordenes final:");
+            foreach (var orden in ordenes)
+            {
+                Console.WriteLine(orden.ToString());
+            }
+        }
+    }
